ccf4dff2562e0e0717b76251645168089db23079
Amer Eid <amereid92@gmail.com>
2022-08-20 19:32:48 +0400

    start vfs crap

--- src/main.js
+++ src/main.js
@@ -3,101 +3,42 @@ const { writeFileSync, readFileSync } = require('fs');
 const puppeteer = require('puppeteer');
 const jsdom = require('jsdom');
 const nodeFetch = require('node-fetch');
-const { getZipCode, getNeighbourhoodData, convertResidentsToPercentage} = require('./utils/utils');
 
-const WIDTH = 1920;
-const HEIGHT = 1080;
 
-const data = readFileSync('db.json', { encoding:'utf8', flag: 'r' });
-const pastResults = new Set(JSON.parse(data) || []);
-console.log('pastResults:', pastResults);
-const newResults = new Set();
+const WIDTH = 1300;
+const HEIGHT = 800;
+
 const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
-const urls = [
-    'https://www.funda.nl/en/koop/amsterdam/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
-    'https://www.funda.nl/en/koop/haarlem/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
-];
 
 const runTask = async () => {
-    for (const url of urls) {
-        await runPuppeteer(url);
-    }
-
-    console.log('newResults:', newResults);
-
-    if (newResults.size > 0) {
-        writeFileSync('db.json', JSON.stringify(Array.from([
-            ...newResults,
-            ...pastResults,
-        ])));
-
-        console.log('sending messages to Telegram');
-        const date = (new Date()).toISOString().split('T')[0];
-        houses.forEach(({
-            path,
-            income,
-            residentsAge0to14,
-            residentsAge15to24,
-            residentsAge25to44,
-            residentsAge45to64,
-            residentsAge65AndOlder,
-            householdsWithChildren,
-            shareOfMorocco,
-            shareOfAntillesOrAruba,
-            shareOfSuriname,
-            shareOfTurkey,
-            neighbourhoodName,
-            municipalityName,
-            shareOfNonImmigrants,
-            residentsCount,
-            totalImmigrantsCount,
-        }) => {
-            let text = `New house on ${date}: [click here](${path})`;
-
-            if (income) {
-                let extraStuff = `
-residentsIncome: **${income}**
-neighbourhoodName: **${neighbourhoodName}**
-municipalityName: **${municipalityName}**
-residentsAge0to14: **${residentsAge0to14}**
-residentsAge15to24: **${residentsAge15to24}**
-residentsAge25to44: **${residentsAge25to44}**
-residentsAge45to64: **${residentsAge45to64}**
-residentsAge65AndOlder: **${residentsAge65AndOlder}**
-householdsWithChildren: **${householdsWithChildren}**
-residentsCount: **${residentsCount}**
-totalImmigrantsCount: **${totalImmigrantsCount}**
-shareOfNonImmigrants: **${shareOfNonImmigrants}**
-shareOfMorocco: **${shareOfMorocco}**
-shareOfAntillesOrAruba: **${shareOfAntillesOrAruba}**
-shareOfSuriname: **${shareOfSuriname}**
-shareOfTurkey: **${shareOfTurkey}**
-`;
-                text = `${text}\n${extraStuff}`;
-            }
-
-            nodeFetch(`https://api.telegram.org/bot${BOT_API}/sendMessage`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    text,
-                    chat_id : CHAT_ID,
-                    parse_mode : 'markdown',
-                }),
-            });
-        });
-    }
+    // await runPuppeteer("https://visa.vfsglobal.com/are/en/nld/login");
+    await runPuppeteer("https://visa.vfsglobal.com/are/en/nld/login");
 };
 
 const runPuppeteer = async (url) => {
+    let conf = new Object();
+
+    let vpn = ["https://uk1785.nordvpn.com:89", 
+    "https://fr822.nordvpn.com:89", 
+    "https://be163.nordvpn.com:89",
+    "https://de1052.nordvpn.com:89",
+    "https://es153.nordvpn.com:89",
+    "https://nl820.nordvpn.com:89"];
+    let chosenserver = rand(vpn);
+    console.log("chosen server: "+chosenserver);
+    // VPN
+    conf.vpnUser   = conf.vpnUSer   || 'AFpn44fbK1haLmTdNQ4ScUPU';
+    conf.vpnPass   = conf.vpnPass   || 'zZkBjbQjXAsKpLiLH6LEqU9W';
+    conf.vpnServer = conf.vpnServer || chosenserver;
+
     console.log('opening headless browser');
     const browser = await puppeteer.launch({
-        headless: true,
-        args: [`--window-size=${WIDTH},${HEIGHT}`],
+        headless: false,
+        args: [`--window-size=${WIDTH},${HEIGHT}`,
+            '--disable-dev-shm-usage',
+            '--proxy-server='+conf.vpnServer],
         defaultViewport: {
             width: WIDTH,
             height: HEIGHT,
@@ -108,76 +49,85 @@ const runPuppeteer = async (url) => {
     // https://stackoverflow.com/a/51732046/4307769 https://stackoverflow.com/a/68780400/4307769
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36');
 
-    console.log('going to funda');
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.authenticate({
+        username: conf.vpnUser,
+        password: conf.vpnPass,
+      });
 
-    const htmlString = await page.content();
-    const dom = new jsdom.JSDOM(htmlString);
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
+    await page.type('#mat-input-0', "amereid922@gmail.com", { delay: 100 });
+    await page.type('#mat-input-1', "Kiss@939", { delay: 100 });
+    const signInButtons = await page.$x('/html/body/app-root/div/app-login/section/div/div/mat-card/form/button')
+    await signInButtons[0].click()
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-    console.log('parsing funda.nl data');
-    const result = dom.window.document.querySelectorAll('.search-result');
-    for (const element of result) {
-        const urlPath = element?.querySelectorAll('a')?.[0]?.href;
-        const headerSubtitle = element?.querySelector('.search-result__header-subtitle');
-        const subtitleText = headerSubtitle?.innerHTML?.trim();
+    const bookButtons = await page.$x('/html/body/app-root/div/app-dashboard/section[1]/div/div[2]/button')
+    await sleep(5000);
+    await bookButtons[0].click()
 
-        let path = urlPath;
-        if (!path.includes('https://www.funda.nl')) {
-            path = `https://www.funda.nl${urlPath}`;
-        }
+    // application center
+    await sleep(3000);
+    var chooseCenters = await page.waitForXPath('/html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[1]/mat-form-field');
+    await chooseCenters.click();
 
-        path = path.replace('?navigateSource=resultlist', '');
-        if (path && !pastResults.has(path) && !newResults.has(path)) {
-            let extraDetails = {};
-            const zipCode = getZipCode(subtitleText || '');
+    var abuDhabi = await page.$x('//*[@id="mat-option-0"]');
+    await abuDhabi[0].click();
+    
+    await sleep(3000);
+    var chooseAppointment = await page.waitForXPath('/html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[2]/mat-form-field');
+    await chooseAppointment.click();
 
-            if (zipCode) {
-                const neighbourhoodData = await getNeighbourhoodData(zipCode);
+    var shotStayVisa = await page.$x('//*[@id="mat-option-2"]');
+    await shotStayVisa[0].click();
 
-                if (neighbourhoodData) {
-                    const residentsCount = neighbourhoodData?.['AantalInwoners_5']?.value || 0;
-                    const westernImmigrantsCount = neighbourhoodData?.['WestersTotaal_17']?.value || 0;
-                    const nonWesternImmigrantsCount = neighbourhoodData?.['NietWestersTotaal_18']?.value || 0;
-                    const totalImmigrantsCount = westernImmigrantsCount + nonWesternImmigrantsCount;
-                    const income = neighbourhoodData?.['GemiddeldInkomenPerInwoner_66']?.value * 1000;
+    await sleep(3000);
+    var chooseCategory = await page.waitForXPath('/html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[3]/mat-form-field');
+    await chooseCategory.click();
 
-                    extraDetails = {
-                        ...extraDetails,
-                        income,
-                        residentsAge0to14: neighbourhoodData['k_0Tot15Jaar_8'].value,
-                        residentsAge15to24: neighbourhoodData['k_15Tot25Jaar_9'].value,
-                        residentsAge25to44: neighbourhoodData['k_25Tot45Jaar_10'].value,
-                        residentsAge45to64: neighbourhoodData['k_45Tot65Jaar_11'].value,
-                        residentsAge65AndOlder: neighbourhoodData['k_65JaarOfOuder_12'].value,
-                        householdsWithChildren: neighbourhoodData['HuishoudensMetKinderen_31'].value,
-                        totalImmigrantsCount,
-                        shareOfMorocco: convertResidentsToPercentage(residentsCount, neighbourhoodData['Marokko_19'].value),
-                        shareOfAntillesOrAruba: convertResidentsToPercentage(residentsCount, neighbourhoodData['NederlandseAntillenEnAruba_20'].value),
-                        shareOfSuriname: convertResidentsToPercentage(residentsCount, neighbourhoodData['Suriname_21'].value),
-                        shareOfTurkey: convertResidentsToPercentage(residentsCount, neighbourhoodData['Turkije_22'].value),
-                        shareOfNonImmigrants: convertResidentsToPercentage(residentsCount, residentsCount - totalImmigrantsCount),
-                        neighbourhoodName: neighbourhoodData.neighbourhoodName.value,
-                        municipalityName: neighbourhoodData.municipalityName.value,
-                        residentsCount,
-                    };
-                }
-            }
+    var subCategoryShortStay = await page.$x('//*[@id="mat-option-4"]');
+    await subCategoryShortStay[0].click();
 
-            newResults.add(path);
-            houses.push({
-                ...extraDetails,
-                path,
-            });
-        }
+    await sleep(5000);
+
+    var result = await page.waitForXPath('/html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[4]/div');
+    let value = await page.evaluate(el => el.textContent, result)
+    console.log(value);
+    if (value != "No appointment slots are currently available. Please try another application centre if applicable") {
+        nodeFetch(`https://api.telegram.org/bot${BOT_API}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                text,
+                chat_id : CHAT_ID,
+                parse_mode : 'markdown',
+            }),
+        });
     }
+    
+// /html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[4]/div
+// Earliest Available Slot : Oct 13, 2022
 
     console.log('closing browser');
     await browser.close();
 };
 
-if (CHAT_ID && BOT_API) {
-    runTask();
-} else {
-    console.log('Missing Telegram API keys!');
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
+
+function rand(items) {
+    // "|" for a kinda "int div"
+    return items[items.length * Math.random() | 0];
+}
+
+
+// if (CHAT_ID && BOT_API) {
+    runTask();
+// } else {
+    // console.log('Missing Telegram API keys!');
+// }
